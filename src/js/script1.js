69574948117df71d4bb5d186faec97b45011fa0b
IvanGlez22 <158486338+IvanGlez22@users.noreply.github.com>
2024-02-06 13:51:54 -0600

    Primeros cambios al proyecto

---
+++ src/js/script1.js
@@ -0,0 +1,30 @@
+
+//mandar mensajes
+
+// si el script tiene alert y prompt del lado del navegador, del lado del servidor dara error
+
+//console.log("Soy un mensaje independiente");
+//alert("Soy un script independiente");
+
+
+
+//pedir nombre con el comando prompt
+//let nombre = prompt("Introduce tu nombre");
+
+//console.log("Tu nombre es: ",nombre);
+
+
+let a=5
+let b=10
+let c=20
+
+
+
+const location1         =           "New York,"         +       "NY";
+const location2= "New York,"+"NY";
+const location3=    "New York,"     +"NY";
+
+
+console.log(location1);
+console.log(location2);
+console.log(location3);
